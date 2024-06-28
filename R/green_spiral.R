fa64c8189766a942f9b29f81dcbfcc523d86304a
nrennie <nrennie35@gmail.com>
2024-06-28 18:07:16 +0100

    Add green spiral

--- R/green_spiral.R
+++ R/green_spiral.R
@@ -1,4 +1,4 @@
-aRt::lines(200, 7, 0.7, main_col = "#B0C0BC", bg_col = "#466365", linewidth = 0.3) + 
+aRt::perpendicular(200, 7, 0.7, main_col = "#B0C0BC", bg_col = "#466365", linewidth = 0.3) + 
   ggplot2::coord_polar()
 
-ggplot2::ggsave("green.png", width = 900, height = 900, units = "px")
+ggplot2::ggsave("Images/green_spiral.png", width = 900, height = 900, units = "px")
