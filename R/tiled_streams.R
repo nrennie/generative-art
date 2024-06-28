3a6f091beb9dfc7bec15b190c38e0165882c81d0
nrennie <nrennie35@gmail.com>
2024-06-28 18:17:36 +0100

    tidy tiled_streams

--- R/tiled_streams.R
+++ R/tiled_streams.R
@@ -1,25 +1,16 @@
 library(aRt)
-set.seed(1234)
-ss <- sample(5:100, size = 25)
-p <- lapply(ss, function(i) streams(type = sample(c("up", "down", "left", "right"), 1),
-                                    s = i))
-patchwork::wrap_plots(p) +
-  patchwork::plot_layout(ncol = 5, nrow = 5) &
-  ggplot2::theme(plot.margin = ggplot2::unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
-                 plot.background = ggplot2::element_rect(fill = "white", colour = "white"),
-                 panel.background = ggplot2::element_rect(fill = "white", colour = "white"))
-
-
 
 set.seed(1234)
 ss <- sample(5:50, size = 16)
 p <- lapply(ss, function(i) streams(type = sample(c("up", "down", "left", "right"), 1),
                                     fill_col = grey.colors(i),
-                                    line_col = NA,
+                                    line_col = "transparent",
                                     bg_col = "black",
                                     s = i))
 patchwork::wrap_plots(p) +
   patchwork::plot_layout(ncol = 4, nrow = 4) &
-  ggplot2::theme(plot.margin = ggplot2::unit(c(0.5, 0.5, 0.5, 0.5), "cm"),
+  ggplot2::theme(plot.margin = ggplot2::unit(c(0.1, 0.1, 0.1, 0.1), "cm"),
                  plot.background = ggplot2::element_rect(fill = "black", colour = "black"),
                  panel.background = ggplot2::element_rect(fill = "black", colour = "black"))
+
+ggplot2::ggsave("Images/tiled_streams.png", width = 900, height = 900, units = "px")
--- README.md
+++ README.md
@@ -3,41 +3,17 @@
 Code for random generative art pieces that don't fit anywhere else...
 
 <p align="center">
-<img src="Images/network.gif" width="96%">
-</p>
-
-<p align="center">
 <img src="Images/dither.png" width="96%">
 </p>
 
 <p align="center">
-<img src="Images/nexus.png" width="96%">
+<img src="Images/sketchy_divide.png" width="32%">
+<img src="Images/network.gif" width="32%">
+<img src="Images/code_golf.png" width="32%">
 </p>
 
 <p align="center">
-<img src="Images/crosshatch.png" width="96%">
-</p>
-
-<p align="center">
-<img src="Images/park.png" width="96%">
-</p>
-
-<p align="center">
-<img src="Images/sketchy_divide_1.png" width="32%">
-<img src="Images/sketchy_divide_2.png" width="32%">
-<img src="Images/sketchy_divide_3.png" width="32%">
-</p>
-
-<p align="center">
-<img src="Images/code_golf_001.png" width="32%">
-<img src="Images/tiled_streams_01.jpeg" width="32%">
-<img src="Images/tiled_streams_02.jpeg" width="32%">
-</p>
-
-<p align="center">
-<img src="Images/circles_twitter_banner.png" width="96%">
-</p>
-
-<p align="center">
-<img src="Images/split_grid.png" width="96%">
+<img src="Images/crosshatch.png" width="32%">
+<img src="Images/nexus.png" width="32%">
+<img src="Images/green_spiral.png" width="32%">
 </p>
